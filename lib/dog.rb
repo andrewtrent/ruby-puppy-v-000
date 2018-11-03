63942e45efc562498f64d3b766edb85152acf3a4
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-03 19:37:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,18 @@ class Dog
     @@all = []
   end
 
+  def all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
 
+  def new(name)
+    @name = name
+    @@all << self
+  end
+
+  def name
+    @name
+  end
 end
